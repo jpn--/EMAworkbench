7fb52fce0cacf17295fbd5073e2adbb4763fc5e8
Jeff Newman <jeff@newman.me>
2019-03-19 21:35:01 -0500

    make Category hashable

--- ema_workbench/em_framework/parameters.py
+++ ema_workbench/em_framework/parameters.py
@@ -66,6 +66,8 @@ class Category(Constant):
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    def __hash__(self):
+        return hash((self.name, self.value))
 
 def create_category(cat):
     if isinstance(cat, Category):
